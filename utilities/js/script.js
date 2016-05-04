feb746b0672c5148e9e53d9c566b82068a5dde4a
hitman <agrimmittal97@gmail.com>
2016-05-04 16:17:24 +0530

    final changes

--- utilities/js/script.js
+++ utilities/js/script.js
@@ -1,155 +1,218 @@
-$(document).ready(function(){
-	$(".login-button").click(function(){
+$(document).ready(function() {
+	$(".login-button").click(function() {
 		var user_id = $(".user-id").val();
 		var pass = $(".password").val();
-		if(user_id == "teacher123" && pass == "teacher123"){
+		if (user_id == "teacher123" && pass == "teacher123") {
 			window.location = "utilities/home.html";
-		}
-		else{
+		} else {
 			$(".alert-row").css({
-				"display":"block"
+				"display": "block"
 			});
 		}
 	});
-		$(".summative").click(function(){
+	$(".summative").click(function() {
 		$(this).addClass('active-assessment');
 		$(".formative").removeClass('active-assessment');
 		$(".summative-box").show();
 		$(".formative-box").hide();
-		
+
 	});
-	$(".formative").click(function(){
+	$(".formative").click(function() {
 		$(".formative").addClass("active-assessment");
 		$(".summative").removeClass('active-assessment');
 		$(".formative-box").show();
 		$(".summative-box").hide();
 	});
-	$(".stats,.news,.suggest").click(function () {
+	$(".head1").click(function() {
+		if ($(".head1 .col-md-1").html() == '<i class="fa fa-chevron-down" aria-hidden="true"></i>') {
+			$(".head1 .col-md-1").html("");
+			$(".head1 .col-md-1").append('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+		} else {
+			$(".head1 .col-md-1").html("");
+			$(".head1 .col-md-1").append('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+		}
+	});
+	$(".head2").click(function() {
+		if ($(".head2 .col-md-1").html() == '<i class="fa fa-chevron-up" aria-hidden="true"></i>') {
+			$(".head2 .col-md-1").html("");
+			$(".head2 .col-md-1").append('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+		} else {
+			$(".head2 .col-md-1").html("");
+			$(".head2 .col-md-1").append('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+		}
+	});
+	$(".head3").click(function() {
+		if ($(".head3 .col-md-1").html() == '<i class="fa fa-chevron-up" aria-hidden="true"></i>') {
+			$(".head3 .col-md-1").html("");
+			$(".head3 .col-md-1").append('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+		} else {
+			$(".head3 .col-md-1").html("");
+			$(".head3 .col-md-1").append('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+		}
+	});
+	$(".head4").click(function() {
+		if ($(".head4 .col-md-1").html() == '<i class="fa fa-chevron-up" aria-hidden="true"></i>') {
+			$(".head4 .col-md-1").html("");
+			$(".head4 .col-md-1").append('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+		} else {
+			$(".head4 .col-md-1").html("");
+			$(".head4 .col-md-1").append('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+		}
+	});
+	$("#unit3").click(function(){
+		$(".content-div-unit3-main").show();
+		$(".content-div-unit3-material").hide();
+$(".content-div-unit3-editmaterial").hide();
+$(".content-div-unit3-strength").hide();
+
+	});
+	$("#material").click(function() {
+		$(".content-div-unit3-main").hide();
+		$(".content-div-unit3-material").show();
+	});
+	$("#edit_material").click(function() {
+		$(".content-div-unit3-material").hide();
+		$(".content-div-unit3-editmaterial").show();
+	});
+	$("#strength").click(function() {
+		$(".content-div-unit3-main").hide();
+		$(".content-div-unit3-material").hide();
+		$(".content-div-unit3-strength").show();
+	});
+	$(".stats,.news,.suggest").click(function() {
 
 		$header = $(this);
-    //getting the next element
-    $content = $header.next();
-    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $content.slideToggle(500, function () {
-        //execute this after slideToggle is done
-        //change text of header based on visibility of content div
-/*        $header.text(function () {
-            //change text based on condition
-            return $content.is(":visible") ? "Collapse" : "Expand";
-        });*/
+        //getting the next element
+        $content = $header.next();
+        //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+        $content.slideToggle(500, function() {
+            //execute this after slideToggle is done
+            //change text of header based on visibility of content div
+            /*        $header.text(function () {
+                        //change text based on condition
+                        return $content.is(":visible") ? "Collapse" : "Expand";
+                    });*/
+                });
     });
-});
 	$.datetimepicker.setLocale('en');
 
-	$('#datetimepicker_format').datetimepicker({value:'2015/04/15 05:03', format: $("#datetimepicker_format_value").val()});
+	$('#datetimepicker_format').datetimepicker({
+		value: '2015/04/15 05:03',
+		format: $("#datetimepicker_format_value").val()
+	});
 	console.log($('#datetimepicker_format').datetimepicker('getValue'));
 
-	$("#datetimepicker_format_change").on("click", function(e){
-		$("#datetimepicker_format").data('xdsoft_datetimepicker').setOptions({format: $("#datetimepicker_format_value").val()});
+	$("#datetimepicker_format_change").on("click", function(e) {
+		$("#datetimepicker_format").data('xdsoft_datetimepicker').setOptions({
+			format: $("#datetimepicker_format_value").val()
+		});
 	});
-	$("#datetimepicker_format_locale").on("change", function(e){
+	$("#datetimepicker_format_locale").on("change", function(e) {
 		$.datetimepicker.setLocale($(e.currentTarget).val());
 	});
 
 	$('#datetimepicker').datetimepicker({
-		dayOfWeekStart : 1,
-		lang:'en',
-		disabledDates:['1986/01/08','1986/01/09','1986/01/10'],
-		startDate:	'1986/01/05'
+		dayOfWeekStart: 1,
+		lang: 'en',
+		disabledDates: ['1986/01/08', '1986/01/09', '1986/01/10'],
+		startDate: '1986/01/05'
 	});
-	$('#datetimepicker').datetimepicker({value:'2015/04/15 05:03',step:10});
+	$('#datetimepicker').datetimepicker({
+		value: '2015/04/15 05:03',
+		step: 10
+	});
 
 	$('.some_class').datetimepicker();
 
 	$('#default_datetimepicker').datetimepicker({
-		formatTime:'H:i',
-		formatDate:'d.m.Y',
-	//defaultDate:'8.12.1986', // it's my birthday
-	defaultDate:'+03.01.1970', // it's my birthday
-	defaultTime:'10:00',
-	timepickerScrollbar:false
-});
+		formatTime: 'H:i',
+		formatDate: 'd.m.Y',
+        //defaultDate:'8.12.1986', // it's my birthday
+        defaultDate: '+03.01.1970', // it's my birthday
+        defaultTime: '10:00',
+        timepickerScrollbar: false
+    });
 
 	$('#datetimepicker10').datetimepicker({
-		step:5,
-		inline:true
+		step: 5,
+		inline: true
 	});
 	$('#datetimepicker_mask').datetimepicker({
-		mask:'9999/19/39 29:59'
+		mask: '9999/19/39 29:59'
 	});
 
 	$('#datetimepicker1').datetimepicker({
-		datepicker:false,
-		format:'H:i',
-		step:5
+		datepicker: false,
+		format: 'H:i',
+		step: 5
 	});
 	$('#datetimepicker2').datetimepicker({
-		yearOffset:222,
-		lang:'ch',
-		timepicker:false,
-		format:'d/m/Y',
-		formatDate:'Y/m/d',
-	minDate:'-1970/01/02', // yesterday is minimum date
-	maxDate:'+1970/01/02' // and tommorow is maximum date calendar
-});
+		yearOffset: 222,
+		lang: 'ch',
+		timepicker: false,
+		format: 'd/m/Y',
+		formatDate: 'Y/m/d',
+        minDate: '-1970/01/02', // yesterday is minimum date
+        maxDate: '+1970/01/02' // and tommorow is maximum date calendar
+    });
 	$('#datetimepicker3').datetimepicker({
-		inline:true
+		inline: true
 	});
 	$('#datetimepicker4').datetimepicker();
-	$('#open').click(function(){
+	$('#open').click(function() {
 		$('#datetimepicker4').datetimepicker('show');
 	});
-	$('#close').click(function(){
+	$('#close').click(function() {
 		$('#datetimepicker4').datetimepicker('hide');
 	});
-	$('#reset').click(function(){
+	$('#reset').click(function() {
 		$('#datetimepicker4').datetimepicker('reset');
 	});
 	$('#datetimepicker5').datetimepicker({
-		datepicker:false,
-		allowTimes:['12:00','13:00','15:00','17:00','17:05','17:20','19:00','20:00'],
-		step:5
+		datepicker: false,
+		allowTimes: ['12:00', '13:00', '15:00', '17:00', '17:05', '17:20', '19:00', '20:00'],
+		step: 5
 	});
 	$('#datetimepicker6').datetimepicker();
-	$('#destroy').click(function(){
-		if( $('#datetimepicker6').data('xdsoft_datetimepicker') ){
+	$('#destroy').click(function() {
+		if ($('#datetimepicker6').data('xdsoft_datetimepicker')) {
 			$('#datetimepicker6').datetimepicker('destroy');
 			this.value = 'create';
-		}else{
+		} else {
 			$('#datetimepicker6').datetimepicker();
 			this.value = 'destroy';
 		}
 	});
-	var logic = function( currentDateTime ){
-		if (currentDateTime && currentDateTime.getDay() == 6){
+	var logic = function(currentDateTime) {
+		if (currentDateTime && currentDateTime.getDay() == 6) {
 			this.setOptions({
-				minTime:'11:00'
+				minTime: '11:00'
 			});
-		}else
+		} else
 		this.setOptions({
-			minTime:'8:00'
+			minTime: '8:00'
 		});
 	};
 	$('#datetimepicker7').datetimepicker({
-		onChangeDateTime:logic,
-		onShow:logic
+		onChangeDateTime: logic,
+		onShow: logic
 	});
 	$('#datetimepicker8').datetimepicker({
-		onGenerate:function( ct ){
+		onGenerate: function(ct) {
 			$(this).find('.xdsoft_date')
 			.toggleClass('xdsoft_disabled');
 		},
-		minDate:'-1970/01/2',
-		maxDate:'+1970/01/2',
-		timepicker:false
+		minDate: '-1970/01/2',
+		maxDate: '+1970/01/2',
+		timepicker: false
 	});
 	$('#datetimepicker9').datetimepicker({
-		onGenerate:function( ct ){
+		onGenerate: function(ct) {
 			$(this).find('.xdsoft_date.xdsoft_weekend')
 			.addClass('xdsoft_disabled');
 		},
-		weekends:['01.01.2014','02.01.2014','03.01.2014','04.01.2014','05.01.2014','06.01.2014'],
-		timepicker:false
+		weekends: ['01.01.2014', '02.01.2014', '03.01.2014', '04.01.2014', '05.01.2014', '06.01.2014'],
+		timepicker: false
 	});
 	var dateToDisable = new Date();
 	dateToDisable.setDate(dateToDisable.getDate() + 2);
@@ -171,7 +234,9 @@ $(document).ready(function(){
 			return [true, ""];
 		}
 	});
-	$('#datetimepicker_dark').datetimepicker({theme:'dark'});
+	$('#datetimepicker_dark').datetimepicker({
+		theme: 'dark'
+	});
 
 
 
