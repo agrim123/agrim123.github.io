cea223e5cf7cccb62331a6913441e65611f8bac1
hitman <agrimmittal97@gmail.com>
2016-05-15 20:22:12 +0530

    sd

--- student-portal/js/script.js
+++ student-portal/js/script.js
@@ -22,12 +22,12 @@ $(document).ready(function(){
 	accordian(".head6",".head6 .col-md-1");
 	accordian(".head7",".head7 .col-md-1");
 	accordian(".head8",".head8 .col-md-1");
-	
+
 	$("#animation").click(function(){
 		$(this).addClass('backcolor');
 		$("#diagram,#formula").removeClass('backcolor');
 		$("#appenddata").html('');
-		$("#appenddata").html('<div class="ques-image"></div>');
+		$("#appenddata").html('<video width="100%" controls><source src="videos/1.mp4" type="video/mp4" /><source src="movie.ogg" type="video/ogg" />Your browser does not support the video tag.</video>');
 	});
 	$("#diagram").click(function(){
 		$(this).addClass('backcolor');
